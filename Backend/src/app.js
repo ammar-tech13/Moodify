0c0a7e05a9da2a64646b42e206e1aa16ec34729f
Ammar <ammartechhub@gmail.com>
2026-06-11 20:18:22 +0530

    add vercel domain

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,12 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-    cors({
-        origin: true,
-        credentials: true
-    })
-);
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://moodify-gamma-orpin.vercel.app"
+    ],
+    credentials: true
+}));
 
 /**
  * Routes
