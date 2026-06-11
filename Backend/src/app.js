d75593a9ee04848312cdfd8de1d06d96ddbbb63c
Ammar <ammartechhub@gmail.com>
2026-06-11 20:08:23 +0530

    fix cors for deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,22 +2,25 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+
+app.use(
+    cors({
+        origin: true,
+        credentials: true
+    })
+);
 
 /**
  * Routes
  */
-const authRoutes = require("./routes/auth.routes")
-const songRoutes = require("./routes/song.routes")
+const authRoutes = require("./routes/auth.routes");
+const songRoutes = require("./routes/song.routes");
 
-app.use("/api/auth", authRoutes)
-app.use("/api/songs", songRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
 
-module.exports = app
+module.exports = app;
